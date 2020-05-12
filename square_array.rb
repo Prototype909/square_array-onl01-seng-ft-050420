61eb29d87bbcd0f737371cbbc2ec74a84a3e249c
Steve Anderson <prototype909@msn.com>
2020-05-12 00:46:45 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,9 @@
 def square_array(array)
   arr = []
-  array.each { |i| arr << i ** 2 }
-  arr
+  # array.each { |i| arr << i ** 2 }
+  array.each do |i|
+    arr.push(i ** 2)
+  end
+
+  return arr
 end
